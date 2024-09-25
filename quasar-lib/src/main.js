2ae469ebb5db7dc1f2dea72a8114ab886834ce14
zhoujing01 <zhoujing01@jsfund.cn>
2024-09-25 15:10:11 +0800

    quasar lib setup

--- quasar-lib/src/main.js
+++ quasar-lib/src/main.js
@@ -1,6 +1,20 @@
 import './assets/main.css'
 
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {
+
+  }
+})
+app.mount('#app')
+
